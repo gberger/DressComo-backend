c64770a5760845df79200dff547159b4b92a4f6b
Guilherme Berger <guilherme.berger@gmail.com>
2015-04-16 13:19:11 -0400

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+gui = User.create({name: 'Guilherme Berger', username: 'gberger', email: 'gberger@unc.edu', password: 'pass1234', location: 'Rio de Janeiro', bio: 'PUC-Rio 2016'})
+harris = User.create({name: 'Harris McClean', username: 'harris', email: 'harris@unc.edu', password: 'pass1234', location: 'Chapel Hill', bio: 'Go Tar Heels!'})
+
+post = Post.create({author: harris, text: '#Everyday Does this look good for a college orientation session?', cloudinary_image_id: 'mirror2_dkzz31'})
+post.liked_by(gui)
